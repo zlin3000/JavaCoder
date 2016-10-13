f6add520f3f29659e62fa5ff3b641bdbe8d51bfc
zlin3000 <lin.zhen@hotmail.com>
2016-10-13 21:56:29 +0800

    add solution of 292 NimGame

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Stack;
 
 public class Main {
-
+	
+	//292. Nim Game
+	static public boolean canWinNim(int n) {
+        if(n <= 3){
+            return true;
+        }
+        else{
+            if(n%4 == 0){
+                return false;
+            }else{
+                return true;
+            }
+        }
+    }
+	
+	
+	static public String convert(String s, int numRows) {
+		
+        return "";
+        
+	}
+        
+        
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
